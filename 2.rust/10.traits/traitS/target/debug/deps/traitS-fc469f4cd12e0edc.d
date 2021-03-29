5027dca2e0961f9b0eefa26f6c8d7220bcf81094
jerome <drchenxie@163.com>
2021-03-29 12:12:51 +0800

    完成了 trait 的学习。

---
+++ 2.rust/10.traits/traitS/target/debug/deps/traitS-fc469f4cd12e0edc.d
@@ -0,0 +1,5 @@
+/home/xiechen/8.cs/2.rust/10.traits/traitS/target/debug/deps/traitS-fc469f4cd12e0edc: src/main.rs
+
+/home/xiechen/8.cs/2.rust/10.traits/traitS/target/debug/deps/traitS-fc469f4cd12e0edc.d: src/main.rs
+
+src/main.rs:
---
+++ 2.rust/10.traits/traitS/target/debug/traitS.d
@@ -0,0 +1,1 @@
+/home/xiechen/8.cs/2.rust/10.traits/traitS/target/debug/traitS: /home/xiechen/8.cs/2.rust/10.traits/traitS/src/main.rs
